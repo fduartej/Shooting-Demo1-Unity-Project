09b76ae6f139b0f36859bb619218c00f9e095543
Fred Duarte <gestion_atlassian@inteligogroup.com>
2020-10-03 13:26:32 -0500

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject winLabel;
+    public Text textScoreValue;
+    int score = 0;
+    bool win = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         InvokeRepeating("Spawn", 1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn(){
+        float randomX = Random.Range(-8f,8f);
+        float ramdomY = Random.Range(-4f, 2f); ;
+
+        Vector3 randomPosition = new Vector3(randomX, ramdomY, 0);
+
+        Instantiate(target, 
+            randomPosition, Quaternion.identity);
+    }
+}
